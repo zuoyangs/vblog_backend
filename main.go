4e445acbc867be3538c762be0e3e88c6cef28e3d
左扬 <1635230007@qq.com>
2023-04-07 18:20:09 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zuoyangs/vblog_backend/blog/http"
+	"github.com/zuoyangs/vblog_backend/blog/impl"
+)
+
+func main() {
+
+	r := gin.Default()
+	//把业务路由通过 web 框架，对外提供服务
+	http.NewHandler(impl.NewImpl()).Registry(r)
+
+}
